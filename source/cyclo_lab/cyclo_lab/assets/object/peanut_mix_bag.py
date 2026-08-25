464265acabc3c9cd43c27eac8eec0c04a9b8ab6a
Doyeon Lee <doylee2003@gmail.com>
2026-08-25 15:25:02 +0900

    Update collision: packages are convex hulls, furnitures made to be better primitive approximations.

--- source/cyclo_lab/cyclo_lab/assets/object/peanut_mix_bag.py
+++ source/cyclo_lab/cyclo_lab/assets/object/peanut_mix_bag.py
@@ -5,7 +5,7 @@ from cyclo_lab.assets.object import CYCLO_LAB_OBJECT_ASSETS_DATA_DIR
 
 PEANUT_MIX_BAG_CFG = RigidObjectCfg(
     spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{CYCLO_LAB_OBJECT_ASSETS_DATA_DIR}/object/peanut_mix_bag.usd",
+        usd_path=f"{CYCLO_LAB_OBJECT_ASSETS_DATA_DIR}/object/peanut_mix_bag_convexhull.usd",
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
             linear_damping=3.0,
--- source/cyclo_lab/cyclo_lab/assets/object/roasted_chestnut_bag.py
+++ source/cyclo_lab/cyclo_lab/assets/object/roasted_chestnut_bag.py
@@ -5,7 +5,7 @@ from cyclo_lab.assets.object import CYCLO_LAB_OBJECT_ASSETS_DATA_DIR
 
 ROASTED_CHESTNUT_BAG_CFG = RigidObjectCfg(
     spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{CYCLO_LAB_OBJECT_ASSETS_DATA_DIR}/object/roasted_chestnut_bag.usd",
+        usd_path=f"{CYCLO_LAB_OBJECT_ASSETS_DATA_DIR}/object/roasted_chestnut_bag_convexhull.usd",
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
             linear_damping=3.0,
